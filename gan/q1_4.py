df24920ee57f299029490f25158fba8a5f980108
sjhpark <jonghoon@andrew.cmu.edu>
2023-10-15 20:14:46 -0400

    Fixed an error.

--- gan/q1_4.py
+++ gan/q1_4.py
@@ -17,9 +17,11 @@ def compute_discriminator_loss(
     # for Q1.5.
     ##################################################################
     '''
-    LSGAN (Least Squares GAN) loss for discriminator (https://arxiv.org/pdf/1611.04076.pdf)
-    discrim_real: D(x)
-    discrim_fake: D(G(z))
+    LSGAN (Least Squares GAN) loss for discriminator
+    Least Squares Generative Adversarial Networks (Mao etclassification al, 2016): https://arxiv.org/pdf/1611.04076.pdf
+    
+    discrim_real: D(x) <-- Discriminator's predicted probability on real data
+    discrim_fake: D(G(z)) <-- Discriminator's predicted probability on fake data
         x: real image
         z: random noise
         D: discriminator network
@@ -43,15 +45,15 @@ def compute_generator_loss(discrim_fake):
     # TODO: 1.4: Implement LSGAN loss for generator.
     ##################################################################
     '''
-        discrim_fake: D(G(z))
-        z: random noise
-        G: generator network
-        D: discriminator network
+        discrim_fake: D(G(z)) <-- Discriminator's predicted probability on fake data
+            z: random noise
+            G: generator network
+            D: discriminator network
     '''
     def MSE_loss(input, target):
         return torch.mean((input - target)**2)
 
-    fake_real_label = torch.ones(discrim_fake) # [1, 1, 1, ...]; coined as "c" in the paper (labels that Generator wants Discriminator to believe for fake data)
+    fake_real_label = torch.ones_like(discrim_fake) # [1, 1, 1, ...]; coined as "c" in the paper (labels that Generator wants Discriminator to believe for fake data)
     loss = 0.5* MSE_loss(input=discrim_fake, target=fake_real_label)
 
     ##################################################################
@@ -74,6 +76,9 @@ if __name__ == "__main__":
         prefix=prefix,
         gen_loss_fn=compute_generator_loss,
         disc_loss_fn=compute_discriminator_loss,
-        log_period=1000,
+        # log_period=1000,
+        ############ FOR DEBIGGING ############
+        log_period=5,
+        #######################################
         amp_enabled=not args.disable_amp,
     )
