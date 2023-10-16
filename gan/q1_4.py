61011dfaf438625b9236ea9d4461e42e2b5d97e7
sjhpark <jonghoon@andrew.cmu.edu>
2023-10-15 20:04:00 -0400

    Added LSGAN's Discriminator and Generator Losses.

--- gan/q1_4.py
+++ gan/q1_4.py
@@ -16,7 +16,22 @@ def compute_discriminator_loss(
     # Do not use discrim_interp, interp, lamb. They are placeholders
     # for Q1.5.
     ##################################################################
-    loss = None
+    '''
+    LSGAN (Least Squares GAN) loss for discriminator (https://arxiv.org/pdf/1611.04076.pdf)
+    discrim_real: D(x)
+    discrim_fake: D(G(z))
+        x: real image
+        z: random noise
+        D: discriminator network
+        G: generator network
+    '''
+    def MSE_loss(input, target):
+        return torch.mean((input - target)**2)
+
+    real_label = torch.ones_like(discrim_real) # [1, 1, 1, ...]; coined as "b" in the paper
+    fake_label = torch.zeros_like(discrim_fake) # [0, 0, 0, ...]; coined as "a" in the paper
+    loss = 0.5 * MSE_loss(input=discrim_real, target=real_label) + 0.5 * MSE_loss(input=discrim_fake, target=fake_label)
+
     ##################################################################
     #                          END OF YOUR CODE                      #
     ##################################################################
@@ -27,7 +42,18 @@ def compute_generator_loss(discrim_fake):
     ##################################################################
     # TODO: 1.4: Implement LSGAN loss for generator.
     ##################################################################
-    loss = None
+    '''
+        discrim_fake: D(G(z))
+        z: random noise
+        G: generator network
+        D: discriminator network
+    '''
+    def MSE_loss(input, target):
+        return torch.mean((input - target)**2)
+
+    fake_real_label = torch.ones(discrim_fake) # [1, 1, 1, ...]; coined as "c" in the paper (labels that Generator wants Discriminator to believe for fake data)
+    loss = 0.5* MSE_loss(input=discrim_fake, target=fake_real_label)
+
     ##################################################################
     #                          END OF YOUR CODE                      #
     ##################################################################
